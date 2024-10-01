71669bf63e2212bff7ea7f164abbb4bbfdc33654
Barbiet <bogdanvinokurov2000@gmail.com>
2024-10-01 19:45:25 +0300

    2

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -6,10 +6,11 @@ import {
   updateContact,
 } from '../services/contacts.js';
 import createHttpError from 'http-errors';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 
 export const getContactsController = async (req, res, next) => {
-  const contacts = await getAllContacts();
-
+  const { page, perPage } = parsePaginationParams(res.query);
+  const contacts = await getAllContacts({ page, perPage });
   res.json({
     status: 200,
     message: 'Successfully found contacts!',
